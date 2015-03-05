2eb8c09cea545ee55788bc6c3e765f678cfb1c58
yordan Kirov <yordan.kirov@etu.u-bordeaux.fr>
2015-03-05 16:03:48 +0000

    IA ne pas toucher

--- 2048/Projet-IA/main-IAYordan.c
+++ 2048/Projet-IA/main-IAYordan.c
@@ -26,26 +26,25 @@ int main(int argc,char **argv){
     grid g= new_grid();
     add_tile(g);
     add_tile(g);
-    //int ch=0;
     grid test = new_grid();
-     tabBestM tab = new_tabBestM();
-     while(!game_over(g)){
-       tab.score = grid_score(test);
-       for(dir i = UP; i<=RIGHT;i++){
-	 copy_grid(g,test);
-	 play(test,i);
-	 play(test,best_move(test));
-	 if(tab.score < grid_score(test)){
-	   tab.score = grid_score(test);
-	   tab.best = i;
-	 }
-	 dir b = tab.best;
-	 play(g,b);
-       }
-       printf("Tile max = %ld \n", maximum_tile(g));
-       delete_grid(g);
-       n-=1;
-     }
+    tabBestM tab = new_tabBestM();
+    while(!game_over(g)){
+      tab.score = grid_score(test);
+      for(dir i = UP; i<=RIGHT;i++){
+	copy_grid(g,test);
+	play(test,i);
+	play(test,best_move(test));
+	if(tab.score < grid_score(test)){
+	  tab.score = grid_score(test);
+	  tab.best = i;
+	}
+	dir b = tab.best;
+	play(g,b);
+      }
+      printf("Tile max = %ld \n", maximum_tile(g));
+      delete_grid(g);
+      n-=1;
+    }
   }
 }
 
@@ -83,19 +82,13 @@ static tabBestM new_tabBestM(){
   return tab;
 }
 
-/* static void delete_tabBestM(tabBestM tab){ */
-/*   free(tab); */
-/* } */
-  
+
 
 static dir best_move(grid g){
   grid test = new_grid();
   tabBestM tab = new_tabBestM();
   boucle_best_move(tab,mouvement_impossible(test),g,test);
-
-  
   dir b = tab.best;
-  //delete_tabBestM(tab);
   delete_grid(test);
   return b;
 }
