371a6e6a8c2b88fffaefd805465fda7dbe59b1de
ysabelle Emery <ysabelle.emery@etu.u-bordeaux.fr>
2015-03-16 16:01:19 +0000

    modif commentaire et indentation

--- 2048/Projet-Jeu/ncurses/main-jeu.c
+++ 2048/Projet-Jeu/ncurses/main-jeu.c
@@ -6,70 +6,69 @@
 #include <ncurses.h>
 #include <curses.h>
 #include <time.h>
-#include <math.h>
 
 static void display_grid(grid g);
-static void display_gameOver(bool *continuer,int *reponse_valide);
+static void display_gameOver(bool *continuer, int *reponse_valide);
 static unsigned long int read_highscore();
 static void write_highscore(unsigned long int score);
 
-int main(int argc,char **argv){
+int main(int argc, char *argv[]){
   keypad(stdscr, TRUE);
-  bool continuer=true;
+  bool continuer = true;
   srand(time(NULL));
-  while (continuer){
+  while(continuer){
     grid g = new_grid();
     add_tile(g);
     add_tile(g);
-    int ch=0;
+    int ch = 0;
     display_grid(g);
     int reponse_valide = 0;
-    while(!game_over(g) && reponse_valide==0){
+    while(!game_over(g) && reponse_valide == 0){
       ch=getch();
       switch(ch){
       case KEY_UP:
-	play(g,UP);
+	play(g, UP);
 	break;
       case KEY_DOWN:
-	play(g,DOWN);
+	play(g, DOWN);
 	break;
       case KEY_RIGHT:
-	play(g,RIGHT);
+	play(g, RIGHT);
 	break;
       case KEY_LEFT:
-	play(g,LEFT);
+	play(g, LEFT);
 	break;
-      case 113: //q pour quitter
-	continuer=false;
-	reponse_valide=1;
+      case 113: // "q" pour quitter
+	continuer = false;
+	reponse_valide = 1;
 	break;
-      case 114: //r pour rejouer
-	reponse_valide=1;
+      case 114: // "r" pour rejouer
+	reponse_valide = 1;
 	break;
       }
       display_grid(g);
     }
-    while (reponse_valide == 0 && continuer==true)
-      display_gameOver(&continuer,&reponse_valide);
+    while(reponse_valide == 0 && continuer == true)
+      display_gameOver(&continuer, &reponse_valide);
     endwin();    
   }
   return EXIT_SUCCESS;
 }
 
-static void display_gameOver(bool *continuer,int *reponse_valide){
+static void display_gameOver(bool *continuer, int *reponse_valide){
   keypad(stdscr, TRUE);
-  int ch=0;  
-  mvprintw(9,45,"GAME OVER");
-  mvprintw(11,36,"Voulez-vous rejouer? y or n ? ");
+  int ch = 0;  
+  mvprintw(9, 45, "GAME OVER");
+  mvprintw(11, 36, "Voulez-vous rejouer? y or n ? ");
   refresh(); 
   cbreak();   
-  ch=getch();
-  if(ch==110){
-    *continuer=false;//arrete le programme, sort de la boucle
+  ch = getch();
+  if(ch == 110){
+    *continuer = false; // Arrete le programme, sort de la boucle
     *reponse_valide = 1;
   }
-  if(ch==121)
-    *reponse_valide = 1;//relance le programme du debut 
+  if(ch == 121)
+    *reponse_valide = 1;// Relance le programme du début 
   endwin();    
 }  
 
@@ -77,48 +76,48 @@ static void display_grid(grid g){
   initscr();
   clear();
   keypad(stdscr, TRUE);
-  // mise en forme de la grille
-  int x=3;
-  int y=2;
-  for (int i=0;i<5;i++){
-    mvhline(x,3,ACS_HLINE,31);
-    x+=4;
+  // Mise en forme de la grille
+  int x = 3;
+  int y = 2;
+  for(int i = 0; i < 5; i++){
+    mvhline(x, 3, ACS_HLINE, 31);
+    x += 4;
   }
-  for(int i=0; i<5; i++){
-    mvvline(4,y,ACS_VLINE,15);
-    y+=8;
+  for(int i = 0; i < 5; i++){
+    mvvline(4, y, ACS_VLINE, 15);
+    y += 8;
   }
   start_color();
-  //definition des couleur (rouge vert jaune bleu fonce violet bleu turquoise blanc)
-  for (int i = 1;i<8;i++){
-    init_pair(i,i,0);
+  // Définition des couleurs (rouge, vert, jaune, bleu fonce, violet, bleu turquoise, blanc)
+  for(int i = 1; i < 8; i++){
+    init_pair(i, i, 0);
   }
-  // mise des valeur dans la grille
-  x=5; 
-  int t[]={1,3,2,6,4,5,7};
-  for (int i=0;i<GRID_SIDE;i++){
-    y=5;
-    for (int j=0;j<GRID_SIDE;j++){
-      if (get_tile(g,i,j)!=0){
-	attron(COLOR_PAIR(t[(get_tile(g,i,j)-1)%7]));
-	//recuperation d'un nombre et coloration
+  // Mise des valeurs dans la grille
+  x = 5; 
+  int t[] = {1,3,2,6,4,5,7};
+  for(int i = 0; i < GRID_SIDE; i++){
+    y = 5;
+    for(int j = 0; j < GRID_SIDE; j++){
+      if(get_tile(g, i, j) != 0){
+	attron(COLOR_PAIR(t[(get_tile(g, i, j) - 1) % 7]));
+	// Récupération d'un nombre et coloration
 	char buff[5];
-	sprintf(buff, "%d",(unsigned int)pow(2, get_tile(g,i,j)));
-	mvprintw(x,y,buff);
+	sprintf(buff, "%d", (unsigned int)pow(2, get_tile(g, i, j)));
+	mvprintw(x, y, buff);
 	attroff(COLOR_PAIR(1));
       }
-      y+=8;
+      y += 8;
     }
-    x+=4;
+    x += 4;
   }
-  mvprintw(1,15,"2048");
-  mvprintw(5,36,"Score: %lu ",grid_score(g));
+  mvprintw(1, 15, "2048");
+  mvprintw(5, 36, "Score: %lu ", grid_score(g));
   unsigned long int read_highscore();
-  if (grid_score(g)>read_highscore()){
+  if(grid_score(g) > read_highscore()){
     write_highscore(grid_score(g));
   }
-  mvprintw(6,36,"High Score: %lu ",read_highscore());
-  mvprintw(21,3,"Recommencer Partie / Quitter Partie (r / q)");
+  mvprintw(6, 36, "High Score: %lu ", read_highscore());
+  mvprintw(21, 3, "Recommencer Partie / Quitter Partie (r / q)");
   refresh();
 }
 
@@ -127,11 +126,10 @@ static unsigned long int read_highscore(){
   FILE *highscore = NULL; //On initialise un pointeur de fichier
   highscore = fopen("../ncurses/highscore_ncurses.txt","r"); //On ouvre HIGHSCORE.txt
   // "r" = read only
-  if (highscore == NULL) //On a pas réussi a ouvrir le fichier
+  if(highscore == NULL) //On a pas réussi a ouvrir le fichier
     return 0;
-  else
-    {
-      fscanf(highscore,"%lu",&score[0]);
+  else{
+      fscanf(highscore, "%lu", &score[0]);
       fclose(highscore);
     }
   return score[0];
@@ -139,8 +137,8 @@ static unsigned long int read_highscore(){
 
 static void write_highscore(unsigned long int score){
   FILE *highscore = NULL;
-  highscore=fopen("../ncurses/highscore_ncurses.txt","w"); //On ouvre HIGHSCORE.txt
+  highscore = fopen("../ncurses/highscore_ncurses.txt","w"); //On ouvre HIGHSCORE.txt
   // "w" = write only => On va écraser le contenu du fichier
-  fprintf(highscore,"%lu",score);
+  fprintf(highscore, "%lu", score);
   fclose(highscore);
 }
--- 2048/Projet-Jeu/src/grid.h
+++ 2048/Projet-Jeu/src/grid.h
@@ -1,5 +1,5 @@
-#ifndef _GRID_H_
-#define _GRID_H_
+#ifndef _GRILLE_H_
+#define _GRILLE_H_
 /**
  * \file grid.h
  * \brief Contains structures and functions needed to play 2048 game.
@@ -39,8 +39,6 @@ typedef unsigned int tile;
  * \brief List of accepted movement in the game
  */
 typedef enum dir_e {UP,LEFT,DOWN,RIGHT} dir;
-
-typedef struct tabBestM tabBestM;
 
 
 
@@ -127,6 +125,7 @@ void add_tile (grid g);
  * \pre the movement d must be possible (i.e. can_move(g,d) == true).
  */
 void play (grid g, dir d);
+
 
 
 #endif
