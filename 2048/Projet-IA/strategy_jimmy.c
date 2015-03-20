c96667e7a023f88ca2190217f9d741429905f825
jimmy Gouraud <jimmy.gouraud@etu.u-bordeaux.fr>
2015-03-20 22:00:55 +0000

    IA-jimmy : 30% de chance de faire 1024, et 1-3chances sur 1000 de faire 2048 !

--- 2048/Projet-IA/strategy_jimmy.c
+++ 2048/Projet-IA/strategy_jimmy.c
@@ -26,82 +26,162 @@ strategy initStructure(){
    == STRATEGIE IA ==
    ================== */
 
-static float poids_fusion (grid g, dir d);
+static float poids_fusion (grid g, dir d, int n);
 static bool possible_lc (grid g, int i, int j, int a,int b);
-static void tri_insertion(float tab[], int taille);
+static void tri_insertion(float tab[], dir tab_dir[], int taille);
 static long maximum_tile (grid g);
 
 static dir strategy_jimmy (grid g){
-  float cpt_up = poids_fusion(g, UP);
-  float cpt_down = poids_fusion(g, DOWN);
-  float cpt_left = poids_fusion(g, LEFT);
-  float cpt_right = poids_fusion(g, RIGHT);
- 
-  if(can_move(g, UP) && can_move(g, LEFT)){
-    if(cpt_up >= cpt_left)
-      return UP;
-    else
-      return LEFT;
-  }else{
-    if(can_move(g, UP) || can_move(g, LEFT)){
-      if(can_move(g, UP))
-	return UP;
-      else
-	return LEFT;
-    }else{
-      if(!possible_lc(g, 0, 0, 1, 0) || !possible_lc(g, 0, 0, 0, 1)){
-	if(can_move(g, RIGHT) && !possible_lc(g, 0, 0, 1, 0))
-	  return RIGHT;
-	else if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 1, 0))
-	  return DOWN;
-	else{
-	  if(cpt_right >= cpt_down)
-	    return RIGHT;
-	  else
-	    return DOWN;
+  float cpt_up = poids_fusion(g, UP, 2);
+  float cpt_down = poids_fusion(g, DOWN, 2);
+  float cpt_left = poids_fusion(g, LEFT, 2);
+  float cpt_right = poids_fusion(g, RIGHT, 2);
+  
+  float tab[4] = {cpt_up, cpt_down, cpt_left, cpt_right};
+  dir tab_dir[4] = {UP, DOWN, LEFT, RIGHT};
+  tri_insertion(tab, tab_dir, 4);
+
+  if(can_move(g, UP)){
+    if(can_move(g, LEFT)){
+      if(can_move(g, RIGHT) && !possible_lc(g, 0, 0, 1, 0)){
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1))
+	  return tab_dir[3];
+	else{ // Si !can_move(g, DOWN) 
+	  if(tab_dir[3] == DOWN)
+	    return tab_dir[2];
+	  return tab_dir[3];
 	}
-      }else{
-	if(cpt_right >= cpt_down)
-	  return RIGHT;
-	else
-	  return DOWN;
+      }else{ //si !can_move(g, RIGHT)
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1)){
+	  if(tab_dir[3] == RIGHT)
+	    return tab_dir[2];
+	  return tab_dir[3];
+	}else{ // si !can_move(g, RIGHT) && !can_move(g, DOWN)
+	  if(tab_dir[3] == DOWN || tab_dir[3] == RIGHT){
+	    if(tab_dir[2] == DOWN || tab_dir[2] == RIGHT)
+	      return tab_dir[1];
+	    return tab_dir[2];
+	  }
+	  return tab_dir[3];
+	}
+      }
+    }else{ // si !can_move(g, LEFT);
+      if(can_move(g, RIGHT) && !possible_lc(g, 0, 0, 1, 0)){
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1)){
+	  if(tab_dir[3] == LEFT)
+	    return tab_dir[2];
+	  return tab_dir[3];
+	}else{ // Si !can_move(g, DOWN) && !can_move(g, LEFT)
+	  if(tab_dir[3] == DOWN || tab_dir[3] == LEFT){
+	    if(tab_dir[2] == DOWN || tab_dir[2] == LEFT)
+	      return tab_dir[1];
+	    return tab_dir[2];
+	  }
+	  return tab_dir[3];
+	}
+      }else{ //si !can_move(g, RIGHT) && !can_move(g, LEFT)
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 1, 0)){
+	  if(tab_dir[3] == RIGHT || tab_dir[3] == LEFT){
+	    if(tab_dir[2] == RIGHT || tab_dir[2] == LEFT)
+	      return tab_dir[1];
+	    return tab_dir[2];
+	  }
+	  return tab_dir[3];
+	}else // si !can_move(g, RIGHT) && !can_move(g, DOWN) && !can_move(g, LEFT)
+	  return UP;
       }
     }
-  }
-
-  /*
-  float tab[4] = {cpt_up, cpt_down, cpt_left, cpt_right};
-  tri_insertion(tab, 4);
-  for(int i = 0; i < 4; i++){
-    if(tab[i] == cpt_up)
-      if(can_move(g, UP))
-	return UP;
-    if(tab[i] == cpt_down)
-      if(can_move(g, DOWN))
+  }else{ // si !can-move(g, UP)
+    if(can_move(g, LEFT)){
+      if(can_move(g, RIGHT) && !possible_lc(g, 0, 0, 1, 0)){
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1)){
+	  if(tab_dir[3] == UP)
+	    return tab_dir[2];
+	  return tab_dir[3];
+	}else{ // Si !can_move(g, DOWN) && !possible_lc(g, 0, 0, 1, 0) && !can_move(g, UP)
+	  if(tab_dir[3] == DOWN || tab_dir[3] == UP){
+	    if(tab_dir[2] == DOWN || tab_dir[2] == UP)
+	      return tab_dir[1];
+	    return tab_dir[2];
+	  }
+	  return tab_dir[3];
+	}
+      }else{ //si !can_move(g, RIGHT) && !possible_lc(g, 0, 0, 0, 1) && !can_move(g, UP)
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1)){
+	  if(tab_dir[3] == RIGHT || tab_dir[3] == UP){
+	    if(tab_dir[2] == RIGHT || tab_dir[2] == UP)
+	      return tab_dir[1];
+	    return tab_dir[2];
+	  }
+	  return tab_dir[3];
+	}else // si !can_move(g, RIGHT) && !can_move(g, DOWN) && !can_move(g, UP)
+	  return LEFT;
+      }
+    }else{ // si !can_move(g, LEFT);
+      if(can_move(g, RIGHT) && !possible_lc(g, 0, 0, 1, 0)){
+	if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1)){
+	  if(tab_dir[3] == UP || tab_dir[3] == LEFT){
+	    if(tab_dir[2] == UP || tab_dir[2] == LEFT)
+	      return tab_dir[1];
+	    return tab_dir[2];
+	  }
+	  return tab_dir[3];
+	}else
+	  return RIGHT;
+      }
+      else if(can_move(g, DOWN) && !possible_lc(g, 0, 0, 0, 1))
 	return DOWN;
-    if(tab[i] == cpt_left)
-      if(can_move(g, LEFT))
-	return LEFT;
-    if(tab[i] == cpt_right)
-      if(can_move(g, RIGHT))
+      else if(can_move(g, DOWN) && can_move(g, RIGHT)){
+	if(cpt_right >= cpt_down)
+	  return RIGHT;
+	return DOWN;
+      }
+      else if(can_move(g, RIGHT))
 	return RIGHT;
+      return DOWN;
+    }
   }
-  */
 }
 
-static float poids_fusion (grid g, dir d){  
-  float poids_fusion = 0;
+
+static float poids_fusion (grid g, dir d, int n){  
+  float poids = 0;
   grid g_copy = new_grid();
   copy_grid(g, g_copy);
   do_move(g_copy, d);
-  poids_fusion = grid_score(g_copy) - grid_score(g);
+  poids = grid_score(g_copy) - grid_score(g);
 
+  while(n > 0){
+    n--;
+    float poids_tmp = 0;
+    int tmp = 0;
+    if(can_move(g_copy, UP)){
+      tmp += 1;
+      poids_tmp += poids_fusion(g_copy, UP, n);
+    }
+    if(can_move(g_copy, LEFT)){
+      tmp += 1;
+      poids_tmp += poids_fusion(g_copy, LEFT, n);
+    }
+    if(can_move(g_copy, RIGHT)){
+      tmp += 1;
+      poids_tmp += poids_fusion(g_copy, RIGHT, n);
+    }
+    if(can_move(g_copy, DOWN)){
+      tmp += 1;
+      poids_tmp += poids_fusion(g_copy, DOWN, n);
+    }
+    if(tmp != 0)
+      poids += (poids_tmp / (tmp)) / (n + 1);
+  }
+  
   if(maximum_tile(g) == get_tile(g_copy, 0, 0))
-     poids_fusion += get_tile(g_copy, 0, 0);
+    poids += get_tile(g_copy, 0, 0) * 2;
   if(maximum_tile(g) == get_tile(g_copy, 1, 0))
-    poids_fusion += get_tile(g_copy, 1, 0)/2;
+    poids += get_tile(g_copy, 1, 0);
   if(maximum_tile(g) == get_tile(g_copy, 0, 1))
-     poids_fusion += get_tile(g_copy, 0, 1)/2;
+    poids += get_tile(g_copy, 0, 1);
+
   // -----------
   // On compte le nombre de case vide
   int nb_case_empty = 0;
@@ -111,6 +191,7 @@ static float poids_fusion (grid g, dir d){
 	nb_case_empty += 1;
     }
   }
+  poids += nb_case_empty;
   //----------
   int cpti;
   int cptj;
@@ -122,28 +203,30 @@ static float poids_fusion (grid g, dir d){
 	// On teste si deux cases sur la meme ligne/colonne permettent une fusion.
 	// On teste l'apparition d'un 2 ou d'un 4.
 	// A completer : le cas où il y a 3 fois le meme chiffre (enlever un cas)
+        float tmp = (40 - nb_case_empty*2);
 	while(cpti + 1 < GRID_SIDE){
 	  cpti += 1;
 	  if(get_tile(g_copy, i, j) == get_tile(g_copy, cpti, j))
-	    poids_fusion += get_tile(g_copy, i, j)/2;
+	    poids += get_tile(g_copy, i, j) / tmp;
 	  else if(get_tile(g_copy, cpti, j) == 0){
 	    if(get_tile(g_copy, i, j) == 2)
-	      poids_fusion += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;	    
+	      poids += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;	    
 	    if(get_tile(g_copy, i, j) == 4)
-	      poids_fusion += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
 	  }
 	  else
 	    break;
 	}
+
         while(cpti - 1 >= 0){
 	  cpti -= 1;
 	  if(get_tile(g_copy, i, j) == get_tile(g_copy, cpti, j))
-	    poids_fusion += get_tile(g_copy, i, j)/2;
+	    poids += get_tile(g_copy, i, j) / tmp;
 	  else if(get_tile(g_copy, cpti, j) == 0){
 	    if(get_tile(g_copy, i, j) == 2)
-	      poids_fusion += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;
 	    if(get_tile(g_copy, i, j) == 4)
-	      poids_fusion += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
 	  }
 	  else
 	    break;
@@ -152,12 +235,12 @@ static float poids_fusion (grid g, dir d){
 	while(cptj + 1 < GRID_SIDE){
 	  cptj += 1;
 	  if(get_tile(g_copy, i, j) == get_tile(g_copy, i, cptj))
-	    poids_fusion += get_tile(g_copy, i, j)/2;
+	    poids += get_tile(g_copy, i, j) / tmp;
 	  else if(get_tile(g_copy, cpti, j) == 0){
 	    if(get_tile(g_copy, i, j) == 2)
-	      poids_fusion += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;
 	    if(get_tile(g_copy, i, j) == 4)
-	      poids_fusion += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
 	  } 
 	  else
 	    break;
@@ -166,12 +249,12 @@ static float poids_fusion (grid g, dir d){
         while(cptj - 1 >= 0){
 	  cptj -= 1;
 	  if(get_tile(g_copy, i, j) == get_tile(g_copy, i, cptj))
-	    poids_fusion += get_tile(g_copy, i, j)/2;
+	    poids += get_tile(g_copy, i, j) / tmp;
 	  else if(get_tile(g_copy, cpti, j) == 0){
 	    if(get_tile(g_copy, i, j) == 2)
-	      poids_fusion += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) * 9 / 10) / nb_case_empty;
 	    if(get_tile(g_copy, i, j) == 4)
-	      poids_fusion += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
+	      poids += (get_tile(g_copy, i, j) / 10) / nb_case_empty;
 	  }
 	  else
 	    break;
@@ -179,7 +262,7 @@ static float poids_fusion (grid g, dir d){
       } // fin du premier if
     } // fin du for(int j = 0; ...)
   } // fin du for(int i = 0; ...)
-  return poids_fusion;
+  return poids;
 }
 
 
@@ -191,7 +274,7 @@ static long maximum_tile (grid g);
 
 int main (int argc, char **argv){
 
-  int n = 1000;
+  int n = 100; // Indice de décrémentation boucle while
   int nb_lance = n;
   int cpt_16 = 0;
   int cpt_32 = 0;
@@ -210,9 +293,11 @@ int main (int argc, char **argv){
     add_tile(g);
     
     while(!game_over(g)){
-      play(g, strategy_jimmy(g));
+      if(can_move(g, strategy_jimmy(g))){
+	 play(g, strategy_jimmy(g));
+      }
     }
-    
+
     if(maximum_tile(g) == 4)
       cpt_16 += 1;
     if(maximum_tile(g) == 5)
@@ -228,7 +313,8 @@ int main (int argc, char **argv){
     if(maximum_tile(g) == 10)
       cpt_1024 += 1;
     if(maximum_tile(g) == 11)
-      cpt_1024 += 1;
+      cpt_2048 += 1;
+
     delete_grid(g);
     n -= 1;
   }
@@ -268,46 +354,22 @@ static bool possible_lc (grid g, int i, int j, int a, int b){
   return false;
 }
 
-static void tri_insertion(float tab[], int taille){
-   for(int i = 1; i < taille; i++){
-       float elem = tab[i];
-       int j;
-       for(j = i; j > 0 && tab[j - 1] > elem; j--)
-           tab[j] = tab[j - 1];
-       tab[j] = elem;
-   }
-}
-/*
-static void incrementation(int *i1, int *i2, int incrementationI1, int incrementationI2){
-  *i1 += incrementationI1;
-  *i2 += incrementationI2;
-}
-
-
-static bool possible_lc(grid g, int x,int y, int addx, int addy){
-  int tmpx = x;
-  int tmpy = y;
-  while(x < GRID_SIDE - addx && x >= 0 - addx && y < GRID_SIDE - addy && y >= 0 - addy){
-
-      // Si deux cases collées sont indentiques alors return true
-      if((get_tile(g, x, y) == get_tile(g, x + addx, y + addy) && get_tile(g, x, y) != 0))
-	return true;
-
-      // Si une case vide est suivie d'une case non vide alors return true
-      else if(get_tile(g, x, y) == 0 && get_tile(g, x + addx, y + addy) != 0)
-	return true;
-      incrementation(&x, &y, addx, addy);
+static void tri_insertion(float tab[], dir tab_dir[], int taille){
+  int j;
+  for(int i = 1; i < taille; i++){
+    float elem = tab[i];
+    dir elem_dir = tab_dir[i];
+    for(j = i; j > 0 && tab[j - 1] > elem; j--){
+      tab[j] = tab[j - 1];
+      tab_dir[j] = tab_dir[j - 1];
     }
-
-    // Réinitialiser i ou j et incrémenter l'autre indice pour changer de ligne ou de colonne selon la direction
-    if(addx == -1 || addx == 1)
-      incrementation(&y, &x, 1, tmpx - x); 
-    else
-      incrementation(&x, &y, 1, tmpy - y); 
-  return false;
+    tab_dir[j] = elem_dir;
+    tab[j] = elem;
+  }
 }
 
 
+/*
 static unsigned long int poids_fusion_1 (grid g, dir d){
   grid grid_copy = new_grid();
   copy_grid(g, grid_copy);
