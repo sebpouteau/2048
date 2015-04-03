df807780cf5ca11f92f1b94d6635c09451eee96c
yordan Kirov <yordan.kirov@etu.u-bordeaux.fr>
2015-04-03 10:58:57 +0000

    mod IA

--- 2048/Projet-IA/strategy_yordan.c
+++ 2048/Projet-IA/strategy_yordan.c
@@ -14,12 +14,14 @@ struct tabBestm{
 };
 
 
-#define MONO_WEIGHT 15
-#define MONO_POW 4
+#define MONO_WEIGHT 4
+#define MONO_POW 47
 #define SMOOTH_WEIGHT 40
-#define EMPTY_WEIGHT 50
-#define SCORE_WEIGHT 1
-#define MERGES_WEIGHT 70
+#define EMPTY_WEIGHT 270
+#define MERGES_WEIGHT 700
+#define LOST_PENALTY 20000
+#define SUM_WEIGHT 3.5
+#define SUM_POW 11
 
 
 
@@ -28,89 +30,90 @@ static long maximum_tile(grid g);
 static tabBestM new_tabBestM();
 static dir best_move(grid g);
 static int empty_tiles(grid g);
-static int monotonicity(grid g);
-static int smoothness(grid g);
-static bool max_in_corner(grid g);
-//static grid best_grid(grid g);
-static void fill_tabG(grid g,grid* tab);
-static float eval_tabG(grid g,grid* tab);
+static float monotonicity(grid g);
+static long int smoothness(grid g);
+static void fill_tabG(int empty, grid g, grid* tab);
+static float eval_tabG(int empty,grid* tab);
+static float nb_merges_and_empty(grid g);
+static float eval_branch(grid g);
 
-
-/* int main(int argc,char **argv){ */
+int main(int argc,char **argv){
   
-/*   int cpt_8 = 0; */
-/*   int cpt_16 = 0; */
-/*   int cpt_32 = 0; */
-/*   int cpt_64 = 0; */
-/*   int cpt_128 = 0; */
-/*   int cpt_256 = 0; */
-/*   int cpt_512 = 0; */
-/*   int cpt_1024 = 0; */
-/*   int cpt_2048 = 0; */
-/*   int cpt_4096 = 0; */
+  int cpt_8 = 0;
+  int cpt_16 = 0;
+  int cpt_32 = 0;
+  int cpt_64 = 0;
+  int cpt_128 = 0;
+  int cpt_256 = 0;
+  int cpt_512 = 0;
+  int cpt_1024 = 0;
+  int cpt_2048 = 0;
+  int cpt_4096 = 0;
   
-/*   srand(time(NULL)); */
-/*   for (int j=0; j < 400;j++){ */
-/*     grid g= new_grid(); */
-/*     add_tile(g); */
-/*     add_tile(g); */
-/*     while(!game_over(g)){ */
-/*       if(can_move(g,best_move(g))) */
-/* 	play(g,best_move(g)); */
-/*     } */
-/*     if(maximum_tile(g) == 3) */
-/*       cpt_8 += 1; */
-/*     if(maximum_tile(g) == 4) */
-/*       cpt_16 += 1; */
-/*     if(maximum_tile(g) == 5) */
-/*       cpt_32 += 1; */
-/*     if(maximum_tile(g) == 6) */
-/*       cpt_64 += 1; */
-/*     if(maximum_tile(g) == 7) */
-/*       cpt_128 += 1; */
-/*     if(maximum_tile(g) == 8) */
-/*       cpt_256 += 1; */
-/*     if(maximum_tile(g) == 9) */
-/*       cpt_512 += 1; */
-/*     if(maximum_tile(g) == 10) */
-/*       cpt_1024 += 1; */
-/*     if(maximum_tile(g) == 11) */
-/*       cpt_2048 += 1; */
-/*     if(maximum_tile(g) == 12) */
-/*       cpt_4096 += 1; */
+  srand(time(NULL));
+  for (int j=0; j < 1;j++){
+    grid g= new_grid();
+    add_tile(g);
+    add_tile(g);
+    while(!game_over(g)){
+      dir b = best_move(g);
+      if(!can_move(g,b))
+	continue;
+      play(g,b);
+    }
+    if(maximum_tile(g) == 3)
+      cpt_8 += 1;
+    if(maximum_tile(g) == 4)
+      cpt_16 += 1;
+    if(maximum_tile(g) == 5)
+      cpt_32 += 1;
+    if(maximum_tile(g) == 6)
+      cpt_64 += 1;
+    if(maximum_tile(g) == 7)
+      cpt_128 += 1;
+    if(maximum_tile(g) == 8)
+      cpt_256 += 1;
+    if(maximum_tile(g) == 9)
+      cpt_512 += 1;
+    if(maximum_tile(g) == 10)
+      cpt_1024 += 1;
+    if(maximum_tile(g) == 11)
+      cpt_2048 += 1;
+    if(maximum_tile(g) == 12)
+      cpt_4096 += 1;
     
-/*     delete_grid(g); */
-/*   } */
-/*   printf("\n --------------- \n"); */
-/*   printf("Nombre de fois 8 : %d\n", cpt_8); */
-/*   printf("Nombre de fois 16 : %d\n", cpt_16); */
-/*   printf("Nombre de fois 32 : %d\n", cpt_32); */
-/*   printf("Nombre de fois 64 : %d\n", cpt_64); */
-/*   printf("Nombre de fois 128 : %d\n", cpt_128); */
-/*   printf("Nombre de fois 256 : %d\n", cpt_256); */
-/*   printf("Nombre de fois 512 : %d\n", cpt_512); */
-/*   printf("Nombre de fois 1024 : %d\n", cpt_1024); */
-/*   printf("Nombre de fois 2048 : %d\n", cpt_2048); */
-/*   printf("Nombre de fois 4096 : %d\n", cpt_4096); */
-/* } */
+    //delete_grid(g);
+  }
+  printf("\n --------------- \n");
+  printf("Nombre de fois 8 : %d\n", cpt_8);
+  printf("Nombre de fois 16 : %d\n", cpt_16);
+  printf("Nombre de fois 32 : %d\n", cpt_32);
+  printf("Nombre de fois 64 : %d\n", cpt_64);
+  printf("Nombre de fois 128 : %d\n", cpt_128);
+  printf("Nombre de fois 256 : %d\n", cpt_256);
+  printf("Nombre de fois 512 : %d\n", cpt_512);
+  printf("Nombre de fois 1024 : %d\n", cpt_1024);
+  printf("Nombre de fois 2048 : %d\n", cpt_2048);
+  printf("Nombre de fois 4096 : %d\n", cpt_4096);
+}
 
-  /* static void display_grid(grid g){ */
-  /*   for (int i=0;i<GRID_SIDE;i++){ */
-  /*     for (int j=0;j<GRID_SIDE;j++){ */
-  /*       printf("%d ",get_tile(g,i,j)); */
-  /*     } */
-  /*     printf("\n"); */
-  /*   } */
-  /*   printf("\n"); */
-  /* } */
+/* static void display_grid(grid g){ */
+/*   for (int i=0;i<GRID_SIDE;i++){ */
+/*     for (int j=0;j<GRID_SIDE;j++){ */
+/*       printf("%d ",get_tile(g,i,j)); */
+/*     } */
+/*     printf("\n"); */
+/*   } */
+/*   printf("\n"); */
+/* } */
   
 
-static long maximum_tile(grid g){
-  long max_tile = 2;
+static long int maximum_tile(grid g){
+  long int max_tile = 2;
   for(int i = 0; i<GRID_SIDE; i++){
     for(int j = 0; j< GRID_SIDE; j++){
       if(get_tile(g, i, j)>max_tile)
-	max_tile = get_tile(g, i, j);
+	max_tile = (long int)get_tile(g, i, j);
     }
   }
   return max_tile;
@@ -120,7 +123,7 @@ static long maximum_tile(grid g){
 
 
 static tabBestM new_tabBestM(){
-  tabBestM tab = malloc(sizeof(struct tabBestm));
+  tabBestM tab = malloc(sizeof(tabBestM*));
   assert(tab);
   tab->score = 0;
   tab->best = 0;
@@ -129,67 +132,48 @@ static tabBestM new_tabBestM(){
 
 
 
-static dir best_move(grid g, int depth, tabBestM tab,dir d){
-  if(depth == 0)
-    return tab;
-  if(!can_move(g,d))
-    return best_move(g,depth,tab,d++);
+static dir best_move(grid g){
+  tabBestM tab = new_tabBestM();
   grid test = new_grid();
-  copy_grid(g,test);
-  float s = eval_branch(test);
-  if(s > tab->score){
-    tab->score += s; 
-    tab->best = d;
+  int empty;
+  for(dir i = UP; i<=RIGHT; i++){
+    if(!can_move(g,i))
+       continue;
+    copy_grid(g,test);
+    do_move(test,i);
+    empty = empty_tiles(test);
+    grid * tabG = malloc((empty*2)*sizeof(grid));
+    assert(tabG);
+    fill_tabG(empty,test, tabG);
+    float sc = eval_tabG(empty,tabG);
+    if(sc > tab->score){
+      tab->score += sc;
+      tab->best = i;
+    }
+    /* for(int i = 0;i<empty*2;i++) */
+    /*   delete_grid(tabG[i]); */
+    /* free(tabG); */
   }
-  do_move(test,d);
-  tabBestM left = new_tabBestM();
-  tabBestM right = new_tabBestM();
-  tabBestM up = new_tabBestM();
-  tabBestM down = new_tabBestM();
-  do_move(test,i);
-  grid* tabG = malloc((2*empty_tiles(test))*sizeof(grid*));
-  fill_tabG(test,tabG);
-  //float score_tabG = eval_tabG(test,tabG);
-  for(int j = 0;j<empty_tiles(test)*2;j+){
-    left = best_move(tabG[j],depth-1,tab,LEFT);
-    right = best_move(tabG[j],depth-1,tab,RIGHT);
-    up = best_move(tabG[j],depth-1,tab,UP);
-    down = best_move(tab[j],depth-1,tab,DOWN);
-  }
-  //dir b = tab->best;
-  delete_grid(test);
-  //trqbva da iztriq vs gridove ot tabloto s gridove !!!! 
-  free(left);
-  free(right);
-  free(up);
-  free(down);
-  return tab;
+  
+  dir b = tab->best;
+  //delete_grid(test);
+  free(tab);
+  return b;
 }
 
-/* static dir best_move2(grid g, int depth,){ */
-/*   if (depth == 0) */
-/*     return dir; */
 
-/*   else{ */
-/*     grid * t=generer_grilles(g,depth); */
-/*     dir d = moyenne_mouvements(t); */
-/*     return best_move2(g,depth--) */
-
-/*   } */
-/* } */
-
-static void fill_tabG(grid g,grid* tab){
+static void fill_tabG(int empty, grid g, grid* tab){
   int i = 0;
-  int n = empty_tiles(g)*2-1;
+  int n = empty*2-1;
   while(i < n){
     grid copy = new_grid();
     copy_grid(g,copy);
     for(int j = 0; j < 4; j++){
       for(int k = 0; k < 4; k++){
-	if(get_tile(copy, j, k) == 0){  // da naucha ezika Latex za pisane vmesto word/writer ..
-	  set_tile(copy, j, k, 2);
+	if(get_tile(copy, j, k) == 0){
+	  set_tile(copy, j, k, 1);
 	  tab[i] = copy;
-	  set_tile(copy, j, k, 4);
+	  set_tile(copy, j, k, 2);
 	  tab[n] = copy;
 	  i++;
 	  n--;
@@ -201,19 +185,14 @@ static void fill_tabG(grid g,grid* tab){
 
 
 
-float eval_branch(grid g){
+static float eval_branch(grid g){
   int over = 0;
   if(game_over(g))
-    over = 20000;
-  float mono;
-  if(max_in_corner(g))
-    mono = 200000;
-  else
-    mono = 0;
+    over = LOST_PENALTY;
+  float mono = monotonicity(g);
   float smooth = smoothness(g)*SMOOTH_WEIGHT;
-  float empty = empty_tiles(g)*EMPTY_WEIGHT;
-  float score = grid_score(g) * SCORE_WEIGHT;
-  return mono + smooth + empty + score - over;
+  float nb_merges = nb_merges_and_empty(g);
+  return smooth + nb_merges - mono - over;
 }
 
 
@@ -229,62 +208,75 @@ static int empty_tiles(grid g){
 }
 
 
-
-static int monotonicity(grid g){
-  int variation_sign;
-  int compteur = 0;
-  if(get_tile(g,0,0)>get_tile(g,0,1))
-    variation_sign = 1;
-  else
-    variation_sign = 0;
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-      if(get_tile(g, i, j) < get_tile(g, i, j+1) && variation_sign == 1){
-	variation_sign = 0;
-	compteur += 1;
-      }
-      if(get_tile(g, i, j) > get_tile(g, i, j+1) && variation_sign == 0){
-	variation_sign = 1;
-	compteur += 1;
-      }
-      if(get_tile(g, j, i) < get_tile(g, j+1, i) && variation_sign == 1){
-	variation_sign = 0;
-	compteur += 1;
-      }
-      if(get_tile(g, j, i) > get_tile(g, j+1, i) && variation_sign == 0){
-	variation_sign = 1;
-	compteur += 1;
-      }
-    }
-  }  
-  return compteur*(-1);
+static float monotonicity(grid g){
+  float mono_left = 0;
+  float mono_right = 0;
+  for(int i = 0;i<GRID_SIDE;i++)
+    for(int j = 1;j<GRID_SIDE;j++)
+      if(get_tile(g,i,j-1)>get_tile(g,i,j))
+	mono_left = pow(get_tile(g,i,j-1),MONO_POW) - pow(get_tile(g,i,j),MONO_POW);
+      else
+	mono_right = pow(get_tile(g,i,j),MONO_POW) - pow(get_tile(g,i,j-1),MONO_POW); 
+  if(mono_left > mono_right)
+    return mono_right;
+  return mono_left;
 }
 
-static int smoothness(grid g){
-  int sum = 0;
+
+static long int smoothness(grid g){
+  long int sum = 0;
   for(int i = 0;i<3;i++){
     for(int j = 0;j<3;j++){
       if(get_tile(g,i,j)>=get_tile(g,i,j))
-	sum+=(int)get_tile(g,i,j)-(int)(get_tile(g,i,j+1));
+	sum+=(long int)get_tile(g,i,j)-(int)(get_tile(g,i,j+1));
       else
-	sum+=(int)get_tile(g,i,j+1)-(int)(get_tile(g,i,j));
+	sum+=(long int)get_tile(g,i,j+1)-(int)(get_tile(g,i,j));
       if(get_tile(g,j,i)>=get_tile(g,j+1,i))
-	sum+=(int)get_tile(g,j,i)-(int)(get_tile(g,j+1,i));
+	sum+=(long int)get_tile(g,j,i)-(int)(get_tile(g,j+1,i));
       else
-	sum+=(int)get_tile(g,j+1,i)-(int)(get_tile(g,j,i));
+	sum+=(long int)get_tile(g,j+1,i)-(int)(get_tile(g,j,i));
     }
   }
   return sum;
 }
 
-bool max_in_corner(grid g){
-  long max_tile = maximum_tile(g);
-  return get_tile(g, 0, 0) == max_tile || get_tile(g, 3, 0) == max_tile || get_tile(g, 0, 3) == max_tile || get_tile(g, 3, 3) == max_tile;
+static float nb_merges_and_empty(grid g){
+  long int the_tile;
+  long int prev = 0;
+  int compt = 0;
+  int empty = 0;
+  float sum;
+  int merges = 0;
+  for(int i = 0;i<GRID_SIDE;i++){
+    for(int j = 0;j<GRID_SIDE;j++){
+      the_tile =(long int)get_tile(g,i,j);
+      sum = pow(the_tile,SUM_POW);
+      if(the_tile == 0)
+	empty++;
+      else{
+	if(prev == the_tile)
+	  compt++;
+	else if(compt > 0){
+	  merges += 1 + compt;
+	  compt = 0;
+	}
+	prev = the_tile;
+      }
+    }
+    if(compt > 0)
+      merges += 1 + compt;
+  }
+  return (empty*EMPTY_WEIGHT)+(merges*MERGES_WEIGHT)-(sum*SUM_WEIGHT);
 }
 
-static float eval_tabG(grid g,grid* tab){
+
+/* bool max_in_corner(grid g){ */
+/*   long max_tile = maximum_tile(g); */
+/*   return get_tile(g, 0, 0) == max_tile || get_tile(g, 3, 0) == max_tile || get_tile(g, 0, 3) == max_tile || get_tile(g, 3, 3) == max_tile; */
+/* } */
+
+static float eval_tabG(int empty,grid* tab){
   float score = 0;
-  int empty = empty_tiles(g);
   for(int i = 0; i < empty; i++)
     score += eval_branch(tab[i])*0.9;
   for(int i = empty; i < empty*2; i++)
@@ -292,112 +284,105 @@ static float eval_tabG(grid g,grid* tab){
   return score/(empty*10);
 }  
 
-/* grid best_grid(grid g){ */
-/*   grid test = new_grid(); */
-/*   copy_grid(g,test); */
-/*   for(dir i = UP;i<=RIGHT;i++) */
-/*     return g; */
+
+/* static void display_grid(grid g); */
+/* static void display_gameOver(bool *continuer, int *reponse_valide); */
+
+
+/* int main(int argc, char *argv[]){ */
+/*   keypad(stdscr, TRUE); */
+/*   bool continuer = true; */
+/*   srand(time(NULL)); */
+/*   while(continuer){ */
+/*     grid g = new_grid(); */
+/*     add_tile(g); */
+/*     add_tile(g); */
+/*     int ch = 0; */
+/*     display_grid(g); */
+/*     int reponse_valide = 0; */
+/*     while(!game_over(g) && reponse_valide == 0){ */
+/*             ch=getch(); */
+/*       switch(ch){ */
+/*       case KEY_UP: */
+	
+/* 	  play(g, best_move(g)); */
+
+/* 	break; */
+/*       case 113: //"q" pour quitter */
+/* 	continuer = false; */
+/* 	reponse_valide = 1; */
+/* 	break; */
+/*       case 114: //"r" pour rejouer */
+/* 	reponse_valide = 1; */
+/* 	break; */
+/*       } */
+/*       display_grid(g); */
+/*     } */
+/*     while(reponse_valide == 0 && continuer == true) */
+/*       display_gameOver(&continuer, &reponse_valide); */
+/*     endwin(); */
+/*   } */
+/*   return EXIT_SUCCESS; */
 /* } */
 
 
-static void display_grid(grid g);
-static void display_gameOver(bool *continuer, int *reponse_valide);
+/* static void display_gameOver(bool *continuer, int *reponse_valide){ */
+/*   keypad(stdscr, TRUE); */
+/*   int ch = 0; */
+/*   mvprintw(9, 45, "GAME OVER"); */
+/*   mvprintw(11, 36, "Voulez-vous rejouer? y or n ? "); */
+/*   refresh(); */
+/*   cbreak(); */
+/*   ch = getch(); */
+/*   if(ch == 110){ */
+/*     *continuer = false; // Arrete le programme et sort de la boucle */
+/*     *reponse_valide = 1; */
+/*   } */
+/*   if(ch == 121) */
+/*     *reponse_valide = 1; // Relance le programme du début */
+/*   endwin(); */
+/* } */
 
-
-int main(int argc, char *argv[]){
-  keypad(stdscr, TRUE);
-  bool continuer = true;
-  srand(time(NULL));
-  while(continuer){
-    grid g = new_grid();
-    add_tile(g);
-    add_tile(g);
-    int ch = 0;
-    display_grid(g);
-    int reponse_valide = 0;
-    while(!game_over(g) && reponse_valide == 0){
-            ch=getch();
-      switch(ch){
-      case KEY_UP:
-	
-	  play(g, best_move(g));
-
-	break;
-      case 113: //"q" pour quitter
-	continuer = false;
-	reponse_valide = 1;
-	break;
-      case 114: //"r" pour rejouer
-	reponse_valide = 1;
-	break;
-      }
-      display_grid(g);
-    }
-    while(reponse_valide == 0 && continuer == true)
-      display_gameOver(&continuer, &reponse_valide);
-    endwin();
-  }
-  return EXIT_SUCCESS;
-}
-
-
-static void display_gameOver(bool *continuer, int *reponse_valide){
-  keypad(stdscr, TRUE);
-  int ch = 0;
-  mvprintw(9, 45, "GAME OVER");
-  mvprintw(11, 36, "Voulez-vous rejouer? y or n ? ");
-  refresh();
-  cbreak();
-  ch = getch();
-  if(ch == 110){
-    *continuer = false; // Arrete le programme et sort de la boucle
-    *reponse_valide = 1;
-  }
-  if(ch == 121)
-    *reponse_valide = 1; // Relance le programme du début
-  endwin();
-}
-
-static void display_grid(grid g){
-  initscr();
-  clear();
-  keypad(stdscr, TRUE);
-  // Mise en forme de la grille
-  int x = 3;
-  int y = 2;
-  for(int i = 0; i < 5; i++){
-    mvhline(x, 3, ACS_HLINE, 31);
-    x += 4;
-  }
-  for(int i = 0; i < 5; i++){
-    mvvline(4, y, ACS_VLINE, 15);
-    y += 8;
-  }
-  start_color();
-  // Définition des couleurs (rouge, vert, jaune, bleu foncé, violet, bleu turquoise, blanc)
-  for(int i = 1; i < 8; i++){
-    init_pair(i, i, 0);
-  }
-  // Mise des valeurs dans la grille
-  x = 5;
-  int t[] = {1,3,2,6,4,5,7};
-  for(int i = 0; i < GRID_SIDE; i++){
-    y = 5;
-    for(int j = 0; j < GRID_SIDE; j++){
-      if(get_tile(g, j, i) != 0){
-	attron(COLOR_PAIR(t[(get_tile(g, j, i) - 1) % 7]));
-	// Récupération d'un nombre et de la coloration
-	char buff[5];
-	sprintf(buff, "%d", (unsigned int)pow(2, get_tile(g, j, i)));
-	mvprintw(x, y, buff);
-	attroff(COLOR_PAIR(1));
-      }
-      y += 8;
-    }
-    x += 4;
-  }
-  mvprintw(1, 15, "2048");
-  mvprintw(21, 3, "Recommencer Partie / Quitter Partie (r / q)");
-  refresh();
-}
+/* static void display_grid(grid g){ */
+/*   initscr(); */
+/*   clear(); */
+/*   keypad(stdscr, TRUE); */
+/*   // Mise en forme de la grille */
+/*   int x = 3; */
+/*   int y = 2; */
+/*   for(int i = 0; i < 5; i++){ */
+/*     mvhline(x, 3, ACS_HLINE, 31); */
+/*     x += 4; */
+/*   } */
+/*   for(int i = 0; i < 5; i++){ */
+/*     mvvline(4, y, ACS_VLINE, 15); */
+/*     y += 8; */
+/*   } */
+/*   start_color(); */
+/*   // Définition des couleurs (rouge, vert, jaune, bleu foncé, violet, bleu turquoise, blanc) */
+/*   for(int i = 1; i < 8; i++){ */
+/*     init_pair(i, i, 0); */
+/*   } */
+/*   // Mise des valeurs dans la grille */
+/*   x = 5; */
+/*   int t[] = {1,3,2,6,4,5,7}; */
+/*   for(int i = 0; i < GRID_SIDE; i++){ */
+/*     y = 5; */
+/*     for(int j = 0; j < GRID_SIDE; j++){ */
+/*       if(get_tile(g, j, i) != 0){ */
+/* 	attron(COLOR_PAIR(t[(get_tile(g, j, i) - 1) % 7])); */
+/* 	// Récupération d'un nombre et de la coloration */
+/* 	char buff[5]; */
+/* 	sprintf(buff, "%d", (unsigned int)pow(2, get_tile(g, j, i))); */
+/* 	mvprintw(x, y, buff); */
+/* 	attroff(COLOR_PAIR(1)); */
+/*       } */
+/*       y += 8; */
+/*     } */
+/*     x += 4; */
+/*   } */
+/*   mvprintw(1, 15, "2048"); */
+/*   mvprintw(21, 3, "Recommencer Partie / Quitter Partie (r / q)"); */
+/*   refresh(); */
+/* } */
 
